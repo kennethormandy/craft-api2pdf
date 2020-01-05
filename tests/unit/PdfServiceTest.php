70b553aca0a8340d65c0a6f0c25f7ceff970a1f7
kennethormandy <hello@kennethormandy.com>
2020-01-04 23:30:48 -0800

    Adds basic test

---
+++ tests/unit/PdfServiceTest.php
@@ -0,0 +1,52 @@
+<?php 
+use \Codeception\Test\Unit;
+use kennethormandy\craftapi2pdf\services\PdfService;
+
+class PdfServiceTest extends Unit
+{
+    public $service;
+    public $apiKey = '';
+    
+    protected function _before()
+    {
+      parent::_before(); 
+
+      $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . './../../'); 
+      $dotenv->load();
+
+      $this->service = new PdfService();
+      $this->apiKey = getenv('API2PDF_KEY');
+    }
+
+    protected function _after()
+    {
+    }
+
+    public function testGenerateFromHtml()
+    {
+      $res = $this->service->generateFromHtml('<p>Hello, world</p>', [
+        "apiKey" => $this->apiKey
+      ]);
+
+      codecept_debug($res);
+
+      $this->assertTrue($res['success']);
+      $this->assertContains('url', $res);
+      $this->assertContains('mbIn', $res);
+      $this->assertTrue($res['mbIn'] > 0);
+    }
+    
+    public function testGenerateFromUrl()
+    {
+      $res = $this->service->generateFromUrl('https://example.com', [
+        "apiKey" => $this->apiKey
+      ]);
+
+      codecept_debug($res);
+
+      $this->assertTrue($res['success']);
+      $this->assertContains('url', $res);
+      $this->assertContains('mbIn', $res);
+      $this->assertTrue($res['mbIn'] > 0);
+    }
+}
