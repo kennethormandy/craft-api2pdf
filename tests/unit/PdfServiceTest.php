e10a82a184a565b430675e6acbacbb5c23d71cb9
kennethormandy <hello@kennethormandy.com>
2020-01-16 09:29:42 -0800

    Updates use

--- tests/unit/PdfServiceTest.php
+++ tests/unit/PdfServiceTest.php
@@ -1,5 +1,6 @@
 <?php 
 use \Codeception\Test\Unit;
+use \Dotenv\Dotenv;
 use kennethormandy\craftapi2pdf\services\PdfService;
 
 class PdfServiceTest extends Unit
@@ -11,7 +12,7 @@ class PdfServiceTest extends Unit
     {
       parent::_before(); 
 
-      $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . './../../'); 
+      $dotenv = Dotenv::createImmutable(__DIR__ . './../../'); 
       $dotenv->load();
 
       $this->service = new PdfService();
