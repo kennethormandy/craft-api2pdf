fcb645b6e5877f4bc1aeaf890f2e5a72c8f5a889
Pierre Lemieux <pl@ock.am>
2020-01-21 15:48:25 -0500

    add missing variable to mergeFromUrls service method

--- src/services/PdfService.php
+++ src/services/PdfService.php
@@ -16,7 +16,7 @@ class PdfService extends Component
     {
         parent::init();
     }
-    
+
     private function _getClient($apiKey = null)
     {
         if ($apiKey == null) {
@@ -24,18 +24,18 @@ class PdfService extends Component
             // tests without an API key
             $apiKey = CraftApi2Pdf::$plugin->getSettings()->getApiKey();
         }
-    
+
         return new Api2Pdf($apiKey);
     }
-    
+
     private function _configureClient($options = [])
     {
         // Allow the apiKey to be passed in as an option.
         // This is undocumented, but useful for testing
         $apiKey = isset($options['apiKey']) ? $options['apiKey'] : null;
-    
+
         $apiClient = $this->_getClient($apiKey);
-    
+
         if ($options) {
             if (isset($options["inline"])) {
                 $apiClient->setInline($options["inline"]);
@@ -47,10 +47,10 @@ class PdfService extends Component
                 $apiClient->setOptions($options);
             }
         }
-    
+
         return $apiClient;
     }
-    
+
     private function _formatResponse($resp, $redirect)
     {
         if ($resp) {
@@ -75,7 +75,7 @@ class PdfService extends Component
         ];
         }
     }
-    
+
     public function renderPdfTemplateHtml()
     {
         // https://docs.craftcms.com/v3/extend/updating-plugins.html#rendering-templates
@@ -94,7 +94,7 @@ class PdfService extends Component
         ]);
         \Craft::$app->view->setTemplateMode($oldMode);
         */
-      
+
         // Using templates from the site
         Craft::$app->getView()->setTemplateMode(View::TEMPLATE_MODE_SITE);
 
@@ -106,31 +106,31 @@ class PdfService extends Component
           'body' => 'Testing'
         ]
         );
-      
+
         return $pdfHtml;
     }
-    
+
     public function generateFromUrl(string $url = 'https://example.com', $options = [])
     {
         $apiClient = $this->_configureClient($options);
         $redirect = false;
-    
+
         if (!$url) {
             return [ "success" => false, "error" => "No URL provided." ];
         }
-    
+
         if (strpos($url, 'localhost:') !== false || strpos($url, 'ddev') !== false) {
             return [ "success" => false, "error" => "Invalid URL: local URL provided, which Api2Pdf won’t be able to access: " . $url ];
         }
-    
+
         if (isset($options['redirect'])) {
             $redirect = $options['redirect'];
         }
-    
+
         $resp = $apiClient->headlessChromeFromUrl($url, $options);
         return $this->_formatResponse($resp, $redirect);
     }
-    
+
     public function generateFromHtml(string $html = '', array $options = [])
     {
         // if ($html !== '') {
@@ -142,17 +142,17 @@ class PdfService extends Component
         $pdfHtml = $html;
         // }
         $redirect = false;
-    
+
         $apiClient = $this->_configureClient($options);
-    
+
         if (!$pdfHtml) {
             return [ "success" => false, "error" => "No HTML provided." ];
         }
-    
+
         if (isset($options['redirect'])) {
             $redirect = $options['redirect'];
         }
-    
+
         $resp = $apiClient->headlessChromeFromHtml($pdfHtml, $options);
         return $this->_formatResponse($resp, $redirect);
     }
@@ -160,11 +160,12 @@ class PdfService extends Component
     public function mergeFromUrls(array $urls = [], array $options = [])
     {
         $apiClient = $this->_configureClient($options);
+        $redirect = false;
 
         if (!$urls || 0 >= sizeof($urls)) {
             return [ "success" => false, "error" => "No URLs provided." ];
         }
-      
+
         // TODO Check each URL to make sure it isn’t local
         // if (strpos($url, 'localhost:') !== false || strpos($url, 'ddev') !== false) {
         //   return [ "success" => false, "error" => "Invalid URL: local URL provided, which Api2Pdf won’t be able to access: " . $url ];
